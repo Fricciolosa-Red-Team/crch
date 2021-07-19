5edf6ea42a73c7944c718842a975d73a0e8b1bbf
edoardottt <edoardott@gmail.com>
2021-07-19 10:28:51 +0200

    update

--- monitor.sh
+++ monitor.sh
@@ -4,7 +4,14 @@ do
     cd ~/nuclei-templates
     git pull
     cd $OLD
-    nuclei -t ~/nuclei-templates/takeovers -l subdomains.txt -silent | anew takeovers.txt | notify
+    # Be aware of this:
+    #    - with `| cut -d " " -f 3-` we remove the timestamp section, this means
+    #       we will not have duplicates (duplicate only for the timestamp), but we don't have
+    #       a time information 
+    #    - without that part of course we will have a lot of duplicates (also in term of notifications),
+    #       but we will have a time information
+    #
+    nuclei -t ~/nuclei-templates/takeovers -l subdomains.txt -silent | cut -d " " -f 3- | anew takeovers.txt | notify
 
     # this every 6 hours
     sleep 21600
